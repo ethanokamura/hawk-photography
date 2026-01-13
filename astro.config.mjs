8289f20751ea7d3a4e1d29de96f2ec04de14159a
Ethan <ethanokamura3@gmail.com>
2026-01-12 16:22:49 -0800

    remove gh pages and added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   devToolbar: {
     enabled: false,
   },
-  site: 'https://hawk-photography.github.io',
+
+  site: 'https://hawk-photography.vercel.app',
+  adapter: vercel(),
 });
